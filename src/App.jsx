3499be3058a4ca540ba7f3dc334c6eae5ca85b27
alishahi <shahy8227@gmail.com>
2025-06-04 01:25:16 +0330

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className='bg-[#0e0e0e] min-h-screen text-white'>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
